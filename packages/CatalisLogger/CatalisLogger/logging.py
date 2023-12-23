247e38793c62a226d72b14ee5ce323f4de95a2ae
Positron11 <aarushbeleren11@gmail.com>
2023-12-23 16:53:25 +0000

    UPDATE LOGGER FILE IO HANDLING

--- packages/CatalisLogger/CatalisLogger/logging.py
+++ packages/CatalisLogger/CatalisLogger/logging.py
@@ -1,5 +1,6 @@
 import os
 from time import strftime, gmtime
+from io import TextIOWrapper
 
 
 # catalis logger
@@ -11,20 +12,20 @@ class Logger:
 
 	# constructor
 	def __init__(self, logfile:str):
-		self.logfile = open(logfile, "a+")
+		self._logfile = logfile
 
 
 	# get open logfile path
 	def __str__(self):
-		return self.logfile.name
+		return self._logfile
 
 
 	# check if the last line in the log is terminated
-	def previous_line_terminated(self) -> bool:
+	def previous_line_terminated(self, file:TextIOWrapper) -> bool:
 		# get last char (if possible) to check if previous entry terminated
 		try: 
-			self.logfile.seek(self.logfile.tell() - 1, os.SEEK_SET)
-			return self.logfile.read(1) in ["\n", ""]
+			file.seek(file.tell() - 1, os.SEEK_SET)
+			return file.read(1) in ["\n", ""]
 		
 		except: # beginning of file 
 			return True
@@ -32,23 +33,20 @@ class Logger:
 
 	# logging function
 	def log(self, string:str, level:int=3, force_newline=False, endline:bool=True) -> None:
-		is_newline = self.previous_line_terminated()
-		
-		# prepend timestamp and level if last char newline (ie. this is a new entry)
-		if force_newline or is_newline: out = f"""{strftime("%T", gmtime())} | {self.levels[level]}{string}"""
-		else: out = f"""{string}"""
-		
-		# append newline if log entry is completed, prepend if specified
-		if force_newline and not is_newline: out = "\n" + out
-		print(out, end="\n" if endline else "", file=self.logfile)
+		with open(self._logfile, "a+") as f:
+			is_newline = self.previous_line_terminated(f)
+			
+			# prepend timestamp and level if last char newline (ie. this is a new entry)
+			if force_newline or is_newline: out = f"""{strftime("%T", gmtime())} | {self.levels[level]}{string}"""
+			else: out = f"""{string}"""
+			
+			# append newline if log entry is completed, prepend if specified
+			if force_newline and not is_newline: out = "\n" + out
+			print(out, end="\n" if endline else "", file=f)
 
 
 	# plain print to logfile
 	def print(self, string:str):
-		newline = "\n" * (not self.previous_line_terminated())
-		print(f"""{newline}{string}""", file=self.logfile)
-
-
-	# close logfile
-	def close(self) -> None:
-		self.logfile.close()
+		with open(self._logfile, "a+") as f:
+			newline = "\n" * (not self.previous_line_terminated(f))
+			print(f"""{newline}{string}""", file=f)
