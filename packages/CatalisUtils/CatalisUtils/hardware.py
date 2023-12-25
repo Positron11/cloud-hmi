239ef2acb35d9e73f08b3f42938a936dd60740fb
Positron11 <aarushbeleren11@gmail.com>
2023-12-25 15:42:12 +0000

    ADD LED CONTROLLER OBJECT TO CATALISUTILS

---
+++ packages/CatalisUtils/CatalisUtils/hardware.py
@@ -0,0 +1,23 @@
+import os
+import subprocess as sh
+
+
+# led object class
+class Led():
+	GPIO_OFFSET = 454
+
+	# constructor
+	def __init__(self, pin:int):
+		self.gpio = pin + self.GPIO_OFFSET
+
+		if not os.path.exists(f"/sys/class/gpio/gpio{self.gpio}"):
+			sh.run(f"echo {self.gpio} > /sys/class/gpio/export", shell=True)
+			sh.run(f"echo out > /sys/class/gpio/gpio{self.gpio}/direction", shell=True)
+
+	# turn LED on
+	def on(self):
+		sh.run(f"echo '1' > /sys/class/gpio/gpio{self.gpio}/value", shell=True)
+	
+	# turn LED on
+	def off(self):
+		sh.run(f"echo '0' > /sys/class/gpio/gpio{self.gpio}/value", shell=True)
